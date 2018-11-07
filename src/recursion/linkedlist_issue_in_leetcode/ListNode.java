da044170706abb14c0f53beedc3076005bb43245
Caipeng <icaipeng@gmail.com>
2018-11-07 08:24:54 +0800

    Modified: Add LinkedList issue in LeetCode web site

---
+++ src/recursion/linkedlist_issue_in_leetcode/ListNode.java
@@ -0,0 +1,7 @@
+package recursion.linkedlist_issue_in_leetcode;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ src/recursion/linkedlist_issue_in_leetcode/RemoveElements.java
@@ -0,0 +1,60 @@
+package recursion.linkedlist_issue_in_leetcode;
+
+public class RemoveElements {
+
+    /**
+     * 删除链表中等于给定值 val 的所有节点。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: 1->2->6->3->4->5->6, val = 6
+     * 输出: 1->2->3->4->5
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        //头节点为指定元素
+        while (head != null && head.val == val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = head;
+
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * 使用虚拟头节点
+     */
+    public ListNode removeElementsV2(ListNode head, int val) {
+        //头节点为指定元素
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return dummyHead.next;
+
+    }
+
+
+}
