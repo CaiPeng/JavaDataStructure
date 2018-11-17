c68f1703b51d680d6fcb08908eff71a9b5b729fd
Caipeng <icaipeng@gmail.com>
2018-11-17 19:35:02 +0800

    Add Map interface definition

---
+++ src/map/Map.java
@@ -0,0 +1,20 @@
+package map;
+
+public interface Map<K, V> {
+
+    void add(K k, V v);
+
+    V remove(K k);
+
+    boolean contains(K k);
+
+    V get(K k);
+
+    void set(K k, V v);
+
+    int getSize();
+
+    boolean isEmpty();
+
+}
+
