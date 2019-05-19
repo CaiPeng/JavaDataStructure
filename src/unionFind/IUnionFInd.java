8d9dbb0aae0ffd44d418d2f3a6a14c136e937d27
Caipeng <icaipeng@gmail.com>
2019-05-19 20:44:16 +0800

    [Added] Union Find Interface

---
+++ src/unionFind/IUnionFInd.java
@@ -0,0 +1,24 @@
+package unionFind;
+
+public interface IUnionFInd {
+
+    /**
+     * 两个元素是否所属同一个集合
+     * 是否是可以连接的
+     * <p>
+     * 参数 传递的是 ID
+     */
+    boolean isConnected(int p, int q);
+
+    /**
+     * 合并
+     */
+    void unionElements(int p, int q);
+
+    /**
+     * @return SIZE
+     */
+    int getSize();
+
+
+}
