0ffadd3ac4ecec022e436d2e5e47594d6b04c9ed
Caipeng <icaipeng@gmail.com>
2019-06-16 11:57:58 +0800

    [Avl]   first commit (Add height & balance factor to node)

---
+++ src/avlTree/source/BSTreeMain.java
@@ -0,0 +1,23 @@
+package avlTree.source;
+
+import binarysearchtree.BSTree;
+
+public class BSTreeMain {
+
+    public static void main(String[] args){
+        BSTree<Integer> bsTree = new BSTree<>();
+        int[] nums = {5,3,6,8,4,2};
+        for (int i = 0; i < nums.length; i++) {
+            bsTree.add(nums[i]);
+        }
+//        bsTree.inOrder();
+
+//        bsTree.preOrder();
+//        bsTree.preOrderV2();
+
+        Integer integer = bsTree.max();
+
+        System.out.println(integer);
+
+    }
+}
